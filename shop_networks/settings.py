90033b5a0807e24ef948c04003009dcdcd2c6997
iam-benyamin <benyaminmahmoudyan@gmail.com>
2023-04-09 21:17:15 +0330

    feat/add swagger

--- shop_networks/settings.py
+++ shop_networks/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # thired party
+    'rest_framework',
+    'rest_framework_swagger',
+
+    # custom apps
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'shop_networks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- shop_networks/urls.py
+++ shop_networks/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
+from shop_networks.views import HelloWorldView
+
 
 urlpatterns = [
+    path(
+        'api_shcema',
+        get_schema_view(title='kid\'s 24'),
+        name='api_schema'
+    ),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url': 'api_schema'}
+    ), name='swagger-ui'),
+
     path('admin/', admin.site.urls),
+
+    path('hello-world/', HelloWorldView.as_view(), name='hello_world'),
 ]
